f02ef1e4f196148305ba6f3409e67df1c0576f80
jamesw1123 <jamesw11233@gmail.com>
2023-04-09 08:21:13 -0500

    Trying to get the remote set up and up to date. Should have done this before starting the project.

---
+++ MessageServiceClient/ClientGui.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ServiceModel;
+using System.Windows.Forms;
+
+namespace MessageServiceClientApp
+{
+    public partial class ClientGui : Form, MessageServiceClientApp.MService.IMessageServiceCallback
+    {
+        private MService.MessageServiceClient client;
+        private InstanceContext instanceContext;
+
+        public ClientGui()
+        {
+            InitializeComponent();
+
+            instanceContext = new InstanceContext(this);
+            client = new MService.MessageServiceClient(instanceContext);
+
+            client.Join("user3");
+        }
+
+        public void RecieveMessage(MService.Message message)
+        {
+            txtSndMessage.Text = "RecieveMessage function";
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            var x = client.GetAllMessages("user3");
+            string result = "";
+            foreach (MService.Message msg in x)
+            {
+                result += msg.Mid + " - " + msg.Recipient + " - " + msg.Name + " - " + msg.Address + " - " + msg.Phone + " - " + msg.Email + " - " + msg.Message_Text + " - " + msg.Date_Taken + " - " + msg.Sent_From + " - " + msg.Delivered + " - " + msg.Read + " - " + msg.Deleted + "\r\n";
+            }
+
+            txtSndMessage.Text = result;
+        }
+
+        private void ClientGui_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            client.Leave("user3");
+        }
+
+        private void ClientGui_Load(object sender, EventArgs e)
+        {
+            // read config settings
+            // get the user id
+            // get the ip and port number for the service
+        }
+    }
+}
